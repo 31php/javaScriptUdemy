e6fbbb18d0a07e5460c182a64716ac1341dfbafb
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2025-02-19 22:48:11 -0300

    add uma 3° linha ao cód

--- ex001/consoleLog.js
+++ ex001/consoleLog.js
@@ -1,2 +1,3 @@
 //a funçao console.log serve para exibir coisas na tela
 console.log('bom dia');// o ponto e vírgula é opcional 
+console.log('meu nome é "Phillip" e estou aprendendo js às '+22+'h da noite');
