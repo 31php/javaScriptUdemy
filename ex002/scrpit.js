5db3ef40359f21c41e558aa683160133442d23fc
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2025-02-21 18:52:40 -0300

    criando variavéis

---
+++ ex002/scrpit.js
@@ -0,0 +1,8 @@
+window.alert('bem-vindo ao js!');
+let num1 = Number(window.prompt('Insira um numero'));
+let num2 = Number(window.prompt('Insira outro numero'));
+
+window.document.write(`A soma entre ${num1} e ${num2} é: ${num1 + num2}`);
+window.document.write(`A subtração entre ${num1} e ${num2} é: ${num1 - num2}`);
+window.document.write(`A Multiplicação entre ${num1} e ${num2} é: ${num1 * num2}`);
+window.document.write(`A divisão entre ${num1} e ${num2} é: ${num1 / num2}`);
