3d556918cbd5fa50c9682f7bee15917ca3b25f39
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2025-02-24 22:22:01 -0300

    add mais tags ao arquivo

--- ex002/scrpit.js
+++ ex002/scrpit.js
@@ -2,6 +2,8 @@
 let num1 = Number(window.prompt('Insira um numero'));
 let num2 = Number(window.prompt('Insira outro numero'));*/
 
+let n1 = window.document.getElementsByClassName('numero1')[0];
+
 window.document.write(`A soma entre ${num1} e ${num2} é: ${num1 + num2}`);
 window.document.write(`A subtração entre ${num1} e ${num2} é: ${num1 - num2}`);
 window.document.write(`A Multiplicação entre ${num1} e ${num2} é: ${num1 * num2}`);
