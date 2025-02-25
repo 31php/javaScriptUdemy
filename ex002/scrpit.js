a268a8aed55bd97add77e3aefd1d8360a71c6be9
Phillip Gonçalves Ferreira <66275004+31php@users.noreply.github.com>
2025-02-24 22:10:34 -0300

    add novas tags ao exercicio

--- ex002/scrpit.js
+++ ex002/scrpit.js
@@ -1,6 +1,6 @@
-window.alert('bem-vindo ao js!');
+/*window.alert('bem-vindo ao js!');
 let num1 = Number(window.prompt('Insira um numero'));
-let num2 = Number(window.prompt('Insira outro numero'));
+let num2 = Number(window.prompt('Insira outro numero'));*/
 
 window.document.write(`A soma entre ${num1} e ${num2} é: ${num1 + num2}`);
 window.document.write(`A subtração entre ${num1} e ${num2} é: ${num1 - num2}`);
